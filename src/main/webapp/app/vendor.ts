bdd370cfc5eb89e29b835e58e3938236e1729651
Benny <bcheng96@umd.edu>
2018-07-24 13:57:24 -0400

    Fix bug with 'Done' status filter

--- src/main/webapp/app/vendor.ts
+++ src/main/webapp/app/vendor.ts
@@ -6,7 +6,7 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/throw';
 // jhipster-needle-add-element-to-vendor - JHipster will add new menu items here
-// import './vendor/kukudocs/docxjs/DocxJS.bundle.min.js'
+import './vendor/kukudocs/docxjs/DocxJS.bundle.min.js'
 // import "./vendor/kukudocs/celljs/CellJS.bundle.min.js"
 // import "./vendor/kukudocs/slidejs/SlideJS.bundle.min.js"
 // import "./vendor/kukudocs/pdfjs/PdfJS.bundle.min.js"
