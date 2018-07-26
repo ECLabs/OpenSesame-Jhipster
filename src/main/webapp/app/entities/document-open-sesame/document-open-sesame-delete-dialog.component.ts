0770af48749114383dfaff80ca86603d9a553f65
Benny <bcheng96@umd.edu>
2018-07-26 14:29:58 -0400

    Fixed foriegn key bug with document and comment

--- src/main/webapp/app/entities/document-open-sesame/document-open-sesame-delete-dialog.component.ts
+++ src/main/webapp/app/entities/document-open-sesame/document-open-sesame-delete-dialog.component.ts
@@ -7,6 +7,8 @@ import { JhiEventManager } from 'ng-jhipster';
 import { DocumentOpenSesame } from './document-open-sesame.model';
 import { DocumentOpenSesamePopupService } from './document-open-sesame-popup.service';
 import { DocumentOpenSesameService } from './document-open-sesame.service';
+import { CommentOpenSesameService } from '../comment-open-sesame/comment-open-sesame.service';
+
 
 @Component({
     selector: 'jhi-document-open-sesame-delete-dialog',
@@ -18,6 +20,7 @@ export class DocumentOpenSesameDeleteDialogComponent {
 
     constructor(
         private documentService: DocumentOpenSesameService,
+        private commentService: CommentOpenSesameService,
         public activeModal: NgbActiveModal,
         private eventManager: JhiEventManager
     ) {
@@ -28,12 +31,40 @@ export class DocumentOpenSesameDeleteDialogComponent {
     }
 
     confirmDelete(id: number) {
-        this.documentService.delete(id).subscribe((response) => {
-            this.eventManager.broadcast({
-                name: 'documentListModification',
-                content: 'Deleted an document'
-            });
-            this.activeModal.dismiss(true);
+      var deleteComment = (comments) => {
+        var doneDeleting = false;
+        for(let comment_id in comments){
+          if(id == comments[comment_id].documentId){
+            // console.log(comments)
+             this.commentService.delete(comments[comment_id].id).subscribe((res)=>{
+             // callback after deleting
+           });
+          }
+        }
+          doneDeleting = true;
+          return new Promise((resolve, reject)=>{
+            if(doneDeleting){
+              resolve('Done Deleting!');
+            }else{
+              var reason = new Error('did not finish deleting comments');
+              reject(reason);
+            }
+          })
+    }
+        this.commentService.query().subscribe((res) => {
+            deleteComment(res.body)
+            .then((fullfilled) => {
+              this.documentService.delete(id).subscribe((response) => {
+                  this.eventManager.broadcast({
+                      name: 'documentListModification',
+                      content: 'Deleted an document'
+                  });
+                  this.activeModal.dismiss(true);
+              });
+            })
+            .catch(function(err){
+              console.log(err);
+            })
         });
     }
 }
