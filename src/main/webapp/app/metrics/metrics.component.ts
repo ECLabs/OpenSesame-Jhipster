5d7f4dc0acfb1422d7bcad125773f330169654c3
Benny <bcheng96@umd.edu>
2018-08-21 13:57:52 -0400

    Added metric for created on

--- src/main/webapp/app/metrics/metrics.component.ts
+++ src/main/webapp/app/metrics/metrics.component.ts
@@ -16,9 +16,11 @@ export class MetricsComponent implements OnInit, AfterContentInit {
     documents: any[];
     documents_countries: Object = {};
     documents_status: Object = {};
-    documents_time_elapsed: Object = {};
+    documents_created_on: Object = {};
     country_chart: any = {};
     status_chart: any = {};
+    created_on_chart: any = {};
+
 
     constructor(
         private documentService: DocumentOpenSesameService,
@@ -39,15 +41,16 @@ export class MetricsComponent implements OnInit, AfterContentInit {
         }).subscribe(
             (res: HttpResponse<DocumentOpenSesame[]>) => {
                 this.documents = res.body;
-                res.body.forEach(({ name, country, currstate }) => {
-                    this.filterDocument(name, country, currstate);
+                res.body.forEach(({ name, country, currstate, createdon }) => {
+                    this.filterDocument(name, country, currstate, createdon);
                 });
                 this.createCountryChart(this.documents_countries);
                 this.createStatusChart(this.documents_status);
+                this.createCreatedOnChart(this.documents_created_on);
             });
     }
 
-    filterDocument(name, country, currstate) {
+    filterDocument(name, country, currstate, createdon) {
         // countries
         if (this.documents_countries[country]) {
             this.documents_countries[country].push(name);
@@ -60,6 +63,13 @@ export class MetricsComponent implements OnInit, AfterContentInit {
         } else {
             this.documents_status[currstate] = [name];
         }
+        //created on
+        if (this.documents_created_on[createdon]) {
+            this.documents_created_on[createdon].push(name);
+        } else {
+            this.documents_created_on[createdon] = [name];
+        }
+
     }
 
 
@@ -87,15 +97,13 @@ export class MetricsComponent implements OnInit, AfterContentInit {
             options: {
                 title: {
                     display: true,
-                    text: 'Documents by State'
+                    text: 'Documents by Country'
                 },
                 scales: {
                     yAxes: [{
                         display: true,
                         ticks: {
                             suggestedMin: 0,    // minimum will be 0, unless there is a lower value.
-                            // OR //
-                            beginAtZero: true   // minimum value will be 0.
                         }
                     }]
                 },
@@ -108,12 +116,12 @@ export class MetricsComponent implements OnInit, AfterContentInit {
     createStatusChart(status_documents) {
         let labels = ['CREATED', 'AUTHOR', 'TE', 'CR', 'SIO', 'ER', 'RO', 'TE2', 'DONE'];
         let colors = ['green', 'blue', 'red', 'peru', 'aqua', 'violet', 'purple', 'grey', 'black'];
-        let data = [0,0,0,0,0,0,0,0,0];
+        let data = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
         // key -> status value -> document name
         for (var key in status_documents) {
             let idx = labels.indexOf(key);
-            data[idx]=(status_documents[key].length);
+            data[idx] = (status_documents[key].length);
         }
 
         this.status_chart = new Chart('status_canvas', {
@@ -136,6 +144,63 @@ export class MetricsComponent implements OnInit, AfterContentInit {
                 maintainAspectRatio: false
             }
         });
+    }
+
+    createCreatedOnChart(created_on_documents) {
+        let labels = [];
+        let dates = [];
+        let data = [];
+
+        for (var key in created_on_documents) {
+            let doc_data = {};
+            doc_data['count'] = created_on_documents[key].length;
+            doc_data['date'] = key;
+            dates.push(doc_data);
+
+        }
+        // sort dates
+        dates.sort(function(a, b) {
+            return Date.parse(a.date) - Date.parse(b.date);
+        })
+
+        for (var key in dates) {
+            let date = dates[key].date.split('00:00:00')[0];
+            labels.push(date);
+            data.push(dates[key].count);
+            console.log(data);
+        }
+
+        this.created_on_chart = new Chart('createdon_canvas', {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: '# documents created',
+                    data: data,
+                    fill: false,
+                    borderColor: 'red',
+                    pointBorderColor: 'black',
+                },
+                ]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: 'Documents by Created On'
+                },
+                scales: {
+                    yAxes: [{
+                        display: true,
+                        ticks: {
+                            suggestedMin: 0,    // minimum will be 0, unless there is a lower value.
+                            suggestedMax: 10,
+                        }
+                    }]
+                },
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        })
     }
 
 }
